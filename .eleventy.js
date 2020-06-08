2dbb3556835ab9be0d01bb27ba677b0747a168db
vin-e <vincent@vin-e.com>
2020-06-08 15:57:40 -0700

    minor updates: sitemap.xml, favicon, gh-pages stuff

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,11 @@ const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight")
 const inclusiveLangPlugin = require("@11ty/eleventy-plugin-inclusive-language")
 
 module.exports = function(eleventyConfig) {
+  eleventyConfig.addFilter("dateDisplay", require("./filters/dateDisplay.js"))
+  eleventyConfig.addFilter("valueIfEmpty", require("./filters/valueIfEmpty.js"))
+
+  eleventyConfig.addPassthroughCopy("src/CNAME")
+  eleventyConfig.addPassthroughCopy("src/robots.txt")
   eleventyConfig.addPassthroughCopy("src/images")
   eleventyConfig.addPassthroughCopy("src/css")
   eleventyConfig.addPassthroughCopy("src/scripts")
@@ -15,6 +20,12 @@ module.exports = function(eleventyConfig) {
 
   const markdownEngine = markdownIt({ html: true })
   eleventyConfig.setLibrary('md', markdownEngine)
+
+  // Unsorted items (in whatever order they were added)
+  eleventyConfig.addCollection("allContent", function(collectionApi) {
+    return collectionApi.getAll()
+  })
+
   return {
     dir: {
       input: "src/",
